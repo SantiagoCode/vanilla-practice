4d95b3def77c37c32b2b830741256dd9f10d40e0
Santiago Salazar <santiagosalazar.dev@gmail.com>
2024-03-24 12:20:43 -0400

    open second page functionality

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ import './public/style.css'
 import { setupCounter } from './counter.js'
 
 setupCounter(document.querySelector('#counter'), document.querySelector('#reset'))
+
+document.querySelector('#openNewWindow').addEventListener('click', () => {
+  const currentUrl = window.location.href
+  window.open(currentUrl, '_blank')
+})
